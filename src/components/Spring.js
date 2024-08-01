0686bab0f9fa37b711a5c5628316c7883daedf1d
Mohamed Said Abdulqadir <80471511+mohamed-abdulqadir@users.noreply.github.com>
2024-08-01 13:50:27 +0100

    Update Spring.js
    2024

--- src/components/Spring.js
+++ src/components/Spring.js
@@ -94,7 +94,7 @@ const Spring = () => {
 
     var variables = {
       season: "SPRING",
-      seasonYear: 2023,
+      seasonYear: 2024,
       page: 1,
       sort: search.sortBy || "POPULARITY_DESC",
     };
