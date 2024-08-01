7b8bb4459183690fbece451aafba0330b205897b
Mohamed Said Abdulqadir <80471511+mohamed-abdulqadir@users.noreply.github.com>
2024-08-01 13:48:32 +0100

    Update Summer.js
    2024

--- src/components/Summer.js
+++ src/components/Summer.js
@@ -93,7 +93,7 @@ const Summer = () => {
 
     var variables = {
       season: "SUMMER",
-      seasonYear: 2023,
+      seasonYear: 2024,
       page: 1,
       sort: search.sortBy || "POPULARITY_DESC",
     };
