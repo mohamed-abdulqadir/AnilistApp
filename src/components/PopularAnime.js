68107c50f9c8e941503a7509fd1143b03e886379
Mohamed Said Abdulqadir <80471511+mohamed-abdulqadir@users.noreply.github.com>
2024-08-15 11:03:55 +0100

    Update PopularAnime.js

--- src/components/PopularAnime.js
+++ src/components/PopularAnime.js
@@ -117,12 +117,12 @@ const PopularAnime = () => {
                           {anime.title.english || anime.title.romaji}
                           <span> </span>
                           <a
-                            href={`https://9anime.gs/filter?keyword=${anime.title.romaji}&sengine=all`}
+                            href={`https://anix.to/filter?keyword=${anime.title.romaji}&sengine=all`}
                             target="_blank"
                             rel="noreferrer"
                           >
                             <img
-                              src="https://d4.alternativeto.net/qOUfLCyFQ-70alqi28JbEZrmi9oNnWmaqmmND0fj9m8/rs:fill:280:280:0/g:ce:0:0/YWJzOi8vZGlzdC9pY29ucy85YW5pbWVfMTA3NTM5LnBuZw.png"
+                              src="https://pbs.twimg.com/profile_images/1263003620594708483/znw0-sKS_400x400.jpg"
                               alt=""
                               className="animix"
                             />
