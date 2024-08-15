f782aa3ab8e9fc3e4a6a2396cdd02a2d4ed04da2
Mohamed Said Abdulqadir <80471511+mohamed-abdulqadir@users.noreply.github.com>
2024-08-15 10:59:00 +0100

    Update HomePage.js

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -112,7 +112,7 @@ const HomePage = () => {
                   {trend.title?.english || trend.title?.romaji}
                 </a>
                 <a
-                  href={`https://9anime.gs/filter?keyword=${
+                  href={`https://anix.to/filter?keyword=${
                     trend?.title?.romaji ||
                     trend?.title?.english ||
                     trend?.title?.native
@@ -122,7 +122,7 @@ const HomePage = () => {
                 >
                   <div className={styles.animix}>
                     <img
-                      src="https://d4.alternativeto.net/qOUfLCyFQ-70alqi28JbEZrmi9oNnWmaqmmND0fj9m8/rs:fill:280:280:0/g:ce:0:0/YWJzOi8vZGlzdC9pY29ucy85YW5pbWVfMTA3NTM5LnBuZw.png"
+                      src="https://pbs.twimg.com/profile_images/1263003620594708483/znw0-sKS_400x400.jpg"
                       alt=""
                     />
                   </div>
