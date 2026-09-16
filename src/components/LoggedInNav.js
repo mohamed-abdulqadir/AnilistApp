920c4e05f193e7e2748d09cafe227e320db00616
Mohamed Said Abdulqadir <80471511+mohamed-abdulqadir@users.noreply.github.com>
2026-09-16 12:48:04 +0100

    Update LoggedInNav.js

--- src/components/LoggedInNav.js
+++ src/components/LoggedInNav.js
@@ -25,7 +25,7 @@ const LoggedInNav = ({ info }) => {
                 <span>Home</span>
               </div>
             </Link>
-            <a href={`/summer`}>
+            <a href={`/fall`}>
               <div className={styles.link}>
                 <img
                   src={process.env.PUBLIC_URL + "/seasonal.png"}
